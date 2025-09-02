6df35c00a39cb4032cc023a79d21e9dfd759fd77
Justin Blackwood <justinedwardblackwood@gmail.com>
2025-09-02 18:59:30 -0400

    Also use the SpeechToTextTool

--- Chapter_4/app.py
+++ Chapter_4/app.py
@@ -4,7 +4,7 @@ import requests
 import inspect
 import pandas
 import openpyxl
-from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel, WikipediaSearchTool, VisitWebpageTool
+from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel, WikipediaSearchTool, VisitWebpageTool, SpeechToTextTool
 
 # (Keep Constants as is)
 # --- Constants ---
@@ -25,7 +25,10 @@ class BasicAgent:
         system_prompt = "You are a general AI assistant. I will ask you a question. Report your thoughts, and finish your answer with the following template: [YOUR FINAL ANSWER]. YOUR FINAL ANSWER should be a number OR as few words as possible OR a comma separated list of numbers and/or strings. If you are asked for a number, don't use comma to write your number neither use units such as $ or percent sign unless specified otherwise. If you are asked for a string, don't use articles, neither abbreviations (e.g. for cities), and write the digits in plain text unless specified otherwise. If you are asked for a comma separated list, apply the above rules depending of whether the element to be put in the list is a number or a string."
 
         # Setup an agent using Qwen, and a search tool
-        agent = CodeAgent(name="agent", tools=[DuckDuckGoSearchTool(), WikipediaSearchTool(), VisitWebpageTool()], model=InferenceClientModel(), additional_authorized_imports=[pandas, openpyxl])
+        agent = CodeAgent(name="agent",
+                          tools=[DuckDuckGoSearchTool(), WikipediaSearchTool(), VisitWebpageTool(), SpeechToTextTool()],
+                          model=InferenceClientModel(model_id="openai/gpt-oss-120b"),
+                          additional_authorized_imports=[pandas, openpyxl])
         final_answer = agent.run(f" {system_prompt} Here is the question: {question}", stream=False)
 
         print(f"Agent returning answer: {final_answer}")
